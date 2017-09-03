3a623fa64556b4502ec27a41fcbea45bef362a85
Daniel Mecklenburg Jr <mecklenburgd@gmail.com>
2017-09-03 16:55:33 -0400

    comments

--- vtxedit.pas
+++ vtxedit.pas
@@ -1,5 +1,34 @@
 {
 
+BSD 2-Clause License
+
+Copyright (c) 2017, Daniel Mecklenburg Jr. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+}
+
+{
+
 UTF 8 nas no characters in the  128-191 range
 
 	https://gist.github.com/NuSkooler/a235339cf383759c49c57ee30d34876c?fref=gc&dti=254577004687053
@@ -549,7 +578,7 @@ begin
 	bmp.free;
 
   // get toolbar caption buttons
-  rs := TResourceStream.Create(HInstance, 'BUTTONS2', Windows.RT_RCDATA);
+  rs := TResourceStream.Create(HInstance, 'CAPTIONBUTTONS', Windows.RT_RCDATA);
   bmp := TBGRABitmap.Create(rs);
   rs.Free;
   rect.top := 0; rect.left := 0; rect.Width := 12; rect.Height := 12;
